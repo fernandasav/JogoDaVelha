5ae1d8caf72d1b063899efc5e6a62f7979f87a15
fernanda-savatin <savatinfernanda@gmail.com>
2022-10-06 22:03:12 -0300

    Acrescentando placar

--- src/Principal.java
+++ src/Principal.java
@@ -5,23 +5,22 @@ public class Principal {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String nome2;
+        int placarJogador1 = 0, placarJogador2 = 0;
+
+        System.out.println("------Bem vindo ao jogo da Velha da B3------");
+        System.out.println("Jogador 1, digite seu nome:");
+        String nome1 = scanner.next();
+        System.out.println("O jogador 1 se chama: " + nome1 + " e joga com o 'X'");
+        System.out.println("Jogador 2, digite seu nome:");
+        nome2 = scanner.next();
+        System.out.println("O jogador 2 se chama: " + nome2 + " e joga com o 'O'");
 
         do {
             Matriz[][] jogoDaVelha = new Matriz[3][3];
             Matriz jogo = new Matriz();
             String posicao;
             int valida = 0, Jogadas = 0, jogarNovamente;
-            String nome2;
-
-            System.out.println("------Jogo da Velha da B3------");
-            System.out.println("   ");
-            jogo.Mostrar();
-            System.out.println("Jogador 1, digite seu nome:");
-            String nome1 = scanner.next();
-            System.out.println("O jogador 1 se chama: " + nome1 + " e joga com o 'X'");
-            System.out.println("Jogador 2, digite seu nome:");
-            nome2 = scanner.next();
-            System.out.println("O jogador 2 se chama: " + nome2 + " e joga com o 'O'");
 
             while (true) {
                 do {
@@ -67,8 +66,16 @@ public class Principal {
             if (jogo.Ganhou(Jogadas).equals("EMPATE")) {
                 System.out.println("Empate!");
             } else {
-                System.out.println("O " + jogo.Ganhou(Jogadas) + " venceu!!!");
+                if (jogo.Ganhou(Jogadas).equals("Jogador 1")) {
+                    System.out.println("Jogador 1 " + nome1 + " venceu!!!");
+                    placarJogador1++;
+                } else {
+                    System.out.println("Jogador 2 " + nome2 + " venceu!!!");
+                    placarJogador2++;
+                }
             }
+            System.out.println("O placar é: ");
+            System.out.println(nome1 + ": " + placarJogador1 + " X " + nome2 + ": " + placarJogador2);
             System.out.println("Deseja jogar novamente?");
             while (true) {
                 Scanner input = new Scanner(System.in);
